00fa3c2804045185e5732ee46245a1a1822b692d
ricardopissarra <ricardopissarra@10.120.80.143>
2019-12-02 16:12:43 +0000

    Corrigir constantes; Limitar a quantidade de água que se pode adicionar

--- src/maquinacafe/Delta.java
+++ src/maquinacafe/Delta.java
@@ -13,89 +13,90 @@ import java.util.Scanner;
  * @author ricardopissarra
  */
 public class Delta {
-    
+
     private double agua;
-    private int CURTO = 25;
-    private int NORMAL = 40;
+    private final int CURTO = 25;
+    private final int NORMAL = 40;
     ArrayList listaCafe = new ArrayList();
-    
-    
+
     public void Start() {
         Scanner input = new Scanner(System.in);
-        String opcao = " ";
-        do{
-            
-        System.out.println("Inserir comando: ");
-        opcao = input.nextLine();
-        if (opcao.contains("CAFE ESPRESSO")){
-            cEspresso(opcao);
-        } else if (opcao.contains("CAFE DECAFEINATO")){
-            cDescafeinado(opcao);
-        } else if(opcao.contains("CAFE LUNGO")){
-            cLongo(opcao);
-        }
-        switch (opcao){
-            case "ADD":
-                System.out.println("Quantidade a adicionar?");
-                double a = input.nextDouble();
-                adicionarAgua(a);
-                break;
-            case "AGUA":
-                nivelAgua();
-                break;
-            case "LISTAR":
-                listarCafe();
-                break;
-            case "CURTO":
-                cCurto();
-                break;
-            case "NORMAL":
-                cNormal();
-                break;
-                
-        }
-        } while(!"sair".equals(opcao));
-      
+        String opcao = "";
+        do {
+
+            System.out.println("Inserir comando: ");
+            opcao = input.nextLine();
+            if (opcao.contains("CAFE ESPRESSO")) {
+                cEspresso(opcao);
+            } else if (opcao.contains("CAFE DECAFEINATO")) {
+                cDescafeinado(opcao);
+            } else if (opcao.contains("CAFE LUNGO")) {
+                cLongo(opcao);
+            }
+            switch (opcao) {
+                case "ADD":
+                    System.out.println("Quantidade a adicionar (em ml)?");
+                    double a = input.nextDouble();
+                    adicionarAgua(a);
+                    break;
+                case "AGUA":
+                    nivelAgua();
+                    break;
+                case "LISTAR":
+                    listarCafe();
+                    break;
+                case "CURTO":
+                    cCurto();
+                    break;
+                case "NORMAL":
+                    cNormal();
+                    break;
+
+            }
+        } while (!"sair".equals(opcao));
+
     }
-    
-    public void adicionarAgua (double valor){
+
+    public void adicionarAgua(double valor) {
+        if(valor <= 1000){
         setAgua(valor);
+        } else {
+            System.out.println("> A máquina só pode levar 1L de água (1000 ml)");
+        }
     }
-    
+
     public void cEspresso(String s) {
 
         CafeEspresso ce = new CafeEspresso();
-        if (agua >= 25){
-        agua = agua - 25;
-        ce.setNome(s);
-        ce.informacao();
-        listaCafe.add(ce);} 
-        else {
+        if (agua >= CURTO) {
+            agua = agua - CURTO;
+            ce.setNome(s);
+            ce.informacao();
+            listaCafe.add(ce);
+        } else {
 
             System.out.println("> Não há água suficiente");
-        }   
+        }
     }
-    
+
     public void cDescafeinado(String s) {
 
-        
-        if (agua >= 25) {
-        Descafeinado cd = new Descafeinado();
-        agua = agua - 25;
-        cd.setNome(s);
-        cd.informacao();
-        listaCafe.add(cd);
+        if (agua >= CURTO) {
+            Descafeinado cd = new Descafeinado();
+            agua = agua - CURTO;
+            cd.setNome(s);
+            cd.informacao();
+            listaCafe.add(cd);
         } else {
             System.out.println("> Não há água suficiente");
         }
     }
-    
+
     public void cLongo(String s) {
         Longo cl = new Longo();
-        
-        
-        if (agua >= 40) {
-            agua = agua -40;
+
+        if (agua >= NORMAL) {
+            agua = agua - NORMAL;
             cl.setNome(s);
             cl.informacao();
             listaCafe.add(cl);
@@ -104,26 +105,26 @@ public class Delta {
         }
 
     }
-    
-    public void nivelAgua(){
-       System.out.println("> o reservatorio tem " + getAgua() + " ml");
+
+    public void nivelAgua() {
+        System.out.println("> o reservatorio tem " + getAgua() + " ml");
     }
-    
-    public void listarCafe(){
-        for (int i = 0; i < listaCafe.size(); i++){
+
+    public void listarCafe() {
+        for (int i = 0; i < listaCafe.size(); i++) {
             Cafe c = (Cafe) listaCafe.get(i);
             c.lCapsulas();
         }
     }
-            
+
     public void cCurto() {
         System.out.println("> " + CURTO + " ml");
     }
-    
+
     public void cNormal() {
         System.out.println("> " + NORMAL + " ml");
     }
-    
+
     /**
      * @return the agua
      */
@@ -137,5 +138,5 @@ public class Delta {
     public void setAgua(double agua) {
         this.agua = agua;
     }
-    
+
 }
